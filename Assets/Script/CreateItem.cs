0b0ea8be297d499c58e16dfaea8862433653ac36
Hồ Hải Đăng <72792170+hohaidang2000@users.noreply.github.com>
2022-03-23 23:28:55 +0700

    done scrolling

--- Assets/Script/CreateItem.cs
+++ Assets/Script/CreateItem.cs
@@ -6,50 +6,151 @@ using UnityEngine.UI;
 using System.Linq;
 public class CreateItem : MonoBehaviour
 {
-    public bool checkScrool=false;
+    public bool checkScrool = false;
     public List<Button> buttonList;
-    public int numItems = 1000;
+
     //public Canvas contentCanvas;
     public RectTransform contentPanel;
     public GameObject content;
     public Button button;
     public ScrollRect scrollRect;
-   
+    public bool scrollingDown = false;
+
+    public int numItems = 1000;
+    private int count = 0;
+    public int headDestroy = 0;
+    public int bottomDestoy = 0;
+    private int maxCreate = 3;
+    private int maxCreateBegin = 10;
+    public float timeRemaining = 1;
+    public int buttonDestroyed = 100;
+
+    private float lastScroll;
+    private float currentScroll;
+
     // Start is called before the first frame update
     void Start()
     {
+        
+        lastScroll = scrollRect.verticalNormalizedPosition;
+        currentScroll = scrollRect.verticalNormalizedPosition;
         List<Button> buttonList = new List<Button>();
-        Create();
+        FirstCreate();
+
+    }
+    public void UpdateCurrentScroll()
+    {
+        currentScroll = scrollRect.verticalNormalizedPosition;
+    }
+    public void PutInButton()
+    {
+
     }
 
     // Update is called once per frame
-    public void Create()
+    public void FirstCreate()
     {
-       
-        for (int i = 1; i <= numItems; i++)
+
+        for (int i = 0; i < maxCreateBegin; i++)
         {
+            bottomDestoy += 1;
+            count += 1;
             float y = contentPanel.sizeDelta.y + button.GetComponent<RectTransform>().sizeDelta.y;
-            contentPanel.sizeDelta = new Vector2(contentPanel.sizeDelta.x,y);
+            contentPanel.sizeDelta = new Vector2(contentPanel.sizeDelta.x, y);
             Button temp = Instantiate(button);
             temp.transform.SetParent(content.transform);
-            temp.GetComponent<ButtonDecription>().nameButton = i.ToString();
-            temp.GetComponent<ButtonDecription>().setScale(Random.Range(1,10), Random.Range(1, 10), Random.Range(1, 10));
-            temp.name = i.ToString();
+            temp.GetComponent<ButtonDecription>().nameButton = count.ToString();
+            temp.GetComponent<ButtonDecription>().setScale(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+            temp.name = count.ToString();
             buttonList.Add(temp);
+            //temp.gameObject.SetActive(false);
             //Debug.Log(temp.name);
         }
     }
-    public void Search(string target)
+    public void BackgroundCreate()
+    {
+        if (count < numItems)
+        {
+
+        
+        for (int i = 0; i < maxCreate; i++)
+        {
+            
+            count += 1;
+            float y = contentPanel.sizeDelta.y + button.GetComponent<RectTransform>().sizeDelta.y;
+            contentPanel.sizeDelta = new Vector2(contentPanel.sizeDelta.x, y);
+            Button temp = Instantiate(button);
+            temp.transform.SetParent(content.transform);
+            temp.GetComponent<ButtonDecription>().nameButton = count.ToString();
+            temp.GetComponent<ButtonDecription>().setScale(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+            temp.name = count.ToString();
+            buttonList.Add(temp);
+            temp.gameObject.SetActive(false);
+            //Debug.Log(temp.name);
+        }
+        }
+    }
+    public void Create()
     {
         
+        for (int i = 0; i < maxCreate; i++)
+        {
+            buttonList[bottomDestoy].gameObject.SetActive(true);
+            bottomDestoy += 1;
+        }
+        
+        for (int i = 0; i < maxCreate; i++)
+        {
+            buttonList[headDestroy].gameObject.SetActive(false);
+            headDestroy += 1;
+        }
+        Vector2 po = contentPanel.anchoredPosition;
+        int sum = content.transform.childCount;
        
+        po.y = po.y-90;
+        contentPanel.anchoredPosition = po;
+
+
+    }
+    public void DeCreate()
+    {
+        
+        for (int i = 0; i < maxCreate; i++)
+        {
+            if(bottomDestoy < 0)
+            {
+                bottomDestoy = 0;
+            }
+            buttonList[bottomDestoy].gameObject.SetActive(false);
+            bottomDestoy -= 1;
+
+        }
+        for (int i = 0; i < maxCreate; i++)
+        {
+
+            buttonList[headDestroy].gameObject.SetActive(true);
+            headDestroy -= 1;
+        }
+        buttonList[bottomDestoy].gameObject.SetActive(false);
+        buttonList[headDestroy].gameObject.SetActive(true);
+        Vector2 po = contentPanel.anchoredPosition;
+        po.y = po.y - 90;
+        contentPanel.anchoredPosition = po;
+
+
+    }
+
+    public void Search(string target)
+    {
+
+
         Transform needButton = content.transform.Find(target);
-        if(needButton != null)
+        if (needButton != null)
         {
             Vector3 getPo = needButton.transform.position;
             getPo.y += 10;
- 
-          
+
+
             Debug.Log("ok");
             contentPanel.anchoredPosition =
             (Vector2)scrollRect.transform.InverseTransformPoint(contentPanel.position)
@@ -59,17 +160,60 @@ public class CreateItem : MonoBehaviour
     }
     public void GetBack()
     {
-        Vector3 getPo = buttonList.First<Button>().transform.position;
-        getPo.y += 10;
-        contentPanel.anchoredPosition = (Vector2)scrollRect.transform.InverseTransformPoint(contentPanel.position) -
-            (Vector2)scrollRect.transform.InverseTransformPoint(getPo);
+        count = -1;
     }
+
+
+
     void Update()
     {
-        if (scrollRect.verticalNormalizedPosition <=0.001f)
+
+        if(count < numItems)
         {
-            GetBack();
+            
         }
+
+        /*
+         if ( timeRemaining > 0)
+         {
+             timeRemaining -= Time.deltaTime;
+         }
+                if(scrollRect.verticalNormalizedPosition <= 0.6)
+         {
+             Create();
+             DestroyButtons();
+             timeRemaining = 1;
+
+
+         }
+         if (content.GetComponentsInChildren<Transform>().GetLength(0) >200)
+         {
+             if (scrollRect.verticalNormalizedPosition >= 0.8 && timeRemaining <= 0)
+             {
+                 ReactiveAheadButton();
+                 timeRemaining = 1;
+
+             }
+        }
+        */
+        float mouse = Input.mouseScrollDelta.y;
+        if (mouse > 0)
+        {
+            DeCreate();
+            lastScroll = currentScroll;
+            scrollingDown = false;
+        }
+        else if (mouse < 0)
+        {
+            BackgroundCreate();
+            Create();
+            lastScroll = currentScroll;
+            scrollingDown = true;
+        }
+
+
+
+
 
 
     }
