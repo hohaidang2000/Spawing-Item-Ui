089b1fab6c65440e23293918262de8eb9670c38a
Hồ Hải Đăng <72792170+hohaidang2000@users.noreply.github.com>
2022-03-28 17:12:30 +0700

    fixing resolution

--- Assets/Script/SubcribeScript.cs
+++ Assets/Script/SubcribeScript.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SubcribeScript : MonoBehaviour
 {
     public Sprite choosenImage;
     public GameObject avatar;
+    public TMP_InputField nameInput;
+    public TMP_InputField decritionInput;
+    public TMP_InputField ruleInput;
+
+
     // Start is called before the first frame update
     public void Choose(GameObject button)
     {
@@ -16,5 +22,9 @@ public class SubcribeScript : MonoBehaviour
     public void Choosen()
     {
         avatar.GetComponent<Image>().sprite = choosenImage;
+    }
+    public void Accept()
+    {
+        Debug.Log(nameInput.text);
     }
 }
